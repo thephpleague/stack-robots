d9cb4ca330c761474fd04aa261d576cf823010d3
Pascal Borreli <pascal@borreli.com>
2013-11-01 23:46:14 +0000

    Removed tabs

--- src/League/StackRobots/Robots.php
+++ src/League/StackRobots/Robots.php
@@ -13,42 +13,42 @@ class Robots implements HttpKernelInterface
      */
     private $app;
 
-	/**
-	 * @var string
-	 */
-	private $env;
+    /**
+     * @var string
+     */
+    private $env;
 
-	/**
-	 * @var string
-	 */
-	private $envVar;
+    /**
+     * @var string
+     */
+    private $envVar;
 
-	/**
-	 * Constructor
-	 *
-	 * @param HttpKernelInterface $app
-	 * @param string              $env    The environment variable to compare against.
-	 * @param string              $envVar The environment variable to inspect.
-	 */
-	public function __construct(HttpKernelInterface $app, $env = 'production', $envVar = 'SERVER_ENV')
+    /**
+     * Constructor
+     *
+     * @param HttpKernelInterface $app
+     * @param string              $env    The environment variable to compare against.
+     * @param string              $envVar The environment variable to inspect.
+     */
+    public function __construct(HttpKernelInterface $app, $env = 'production', $envVar = 'SERVER_ENV')
     {
         $this->app = $app;
         $this->env = $env;
-		$this->envVar = $envVar;
+        $this->envVar = $envVar;
     }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true)
+    /**
+     * {@inheritdoc}
+     */
+    public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true)
     {
-		if (getenv($this->envVar) !== $this->env) {
-			// @TODO: Add support for X-Robots-Tag
+        if (getenv($this->envVar) !== $this->env) {
+            // @TODO: Add support for X-Robots-Tag
 
-			if ($request->getPathInfo() === '/robots.txt') {
-				return new Response("User-Agent: *\nDisallow: /", 200, array('Content-Type' => 'text/plain'));
-			}
-		}
+            if ($request->getPathInfo() === '/robots.txt') {
+                return new Response("User-Agent: *\nDisallow: /", 200, array('Content-Type' => 'text/plain'));
+            }
+        }
 
         return $this->app->handle($request, $type, $catch);
     }
